42b830da01b75e5d3af7fd3f21b2f4a00d30bec9
Astrid Ixehuatl <astrid.ixe.gon@gmail.com>
2022-08-02 16:26:17 -0500

    Se termina app CRUD

--- script.js
+++ script.js
@@ -32,14 +32,41 @@ var app = new function() {
     };
 
     this.Edit = function(item){
+        el = document.getElementById('edit-todo');
+        el.value = this.tasks[item]
+        document.getElementById('edit-box').style.display = 'block';
+        self=this;
+
+        document.getElementById('save-edit').onsubmit = function(){
+            var task = el.value;
+            if(task){
+                self.tasks.splice(item, 1, task.trim());
+                self.FetchAll();
+                CloseInput();
+
+            }
+        }
 
     };
 
     this.Delete = function (item){
+        this.tasks.splice(item,1)
+        this.FetchAll();
 
     };
 
     this.Count = function(data){
+        var el = document.getElementById('counter');
+        var name = 'Libros';
+        if(data){
+            if(data == 1){
+                name = 'Libro';
+            }
+            el.innerHTML = data+' '+name;
+        }
+        else{
+            el.innerHTML = "No tienes"+ " " + name;
+        }
 
     };
 
