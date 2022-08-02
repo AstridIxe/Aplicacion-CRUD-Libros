13a0dbf8a115679a403422fb9c464b859076d530
Astrid Ixehuatl <astrid.ixe.gon@gmail.com>
2022-08-02 15:55:34 -0500

    Se agregan botones de edit y delete

--- script.js
+++ script.js
@@ -3,10 +3,31 @@ var app = new function() {
     this.tasks=[]
 
     this.FetchAll = function(){
+        var data='';
+
+        if(this.tasks.length>0){
+            for(i=0;i<this.tasks.length;i++){
+                data+='<tr>';
+                data+='<td>'+(i+1)+'. '+this.tasks[i]+'</td>';
+                data+='<td><button onclick="app.Edit('+i+')" class="btn btn-warning">Editar</button></td> ';
+                data+='<td><button onclick="app.Delete('+i+')" class="btn btn-danger">Borrar</button></td> ';
+                data+='</tr>'
+            }
+        }
+        this.Count(this.tasks.length);
+        return this.el.innerHTML = data
 
     };
 
     this.Add = function(){
+        el = document.getElementById('add-todo');
+        var task = el.value;
+        if(task){
+            this.tasks.push(task.trim());
+            el.value='';
+            this.FetchAll();
+        }
+
 
     };
 
